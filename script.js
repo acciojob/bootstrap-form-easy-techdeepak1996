916cfefbc2d543f87569ce8fc9c4e12528a15faa
Tech Deepak <141490582+techdeepak1996@users.noreply.github.com>
2024-05-09 05:03:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your code here
+document.getElementById('internship-form').addEventListener('submit', function(event) {
+    var form = event.target;
+    if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+    form.classList.add('was-validated');
+});
